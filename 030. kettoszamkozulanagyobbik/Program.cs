f6d572cfaf769741c24ac781d51491040cdcaa1a
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 17:05:35 +0200

    Két szám közül a nagyobbikat írja ki

---
+++ 030. kettoszamkozulanagyobbik/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _030.kettoszamkozulanagyobbik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+
+            if (szam1 > szam2)
+                Console.WriteLine("A nagyobbik szám: " + szam1 + "." + "\n");
+
+            else
+                Console.WriteLine("A nagyobbik szám: " + szam2 + "." + "\n");
+
+            Console.ReadKey();
+        }
+
+    }
+}
